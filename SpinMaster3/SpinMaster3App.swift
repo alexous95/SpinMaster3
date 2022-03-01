f6d439b4ab01d6abdfcb926ce4f6e24da27fad13
Alexandre goncalves <alexandre.goncalves@benextcompany.com>
2022-03-01 17:31:39 +0100

    Initial Commit

---
+++ SpinMaster3/SpinMaster3App.swift
@@ -0,0 +1,17 @@
+//
+//  SpinMaster3App.swift
+//  SpinMaster3
+//
+//  Created by Alexandre G on 01/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct SpinMaster3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
